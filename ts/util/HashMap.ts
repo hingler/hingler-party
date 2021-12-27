149b8cc9b9f1599d77879a663f6b2c86d03c44e8
jamieboy1337 <jamieboy1337@gmail.com>
2021-12-27 10:08:57 -0800

    objmap rolled out

--- ts/util/HashMap.ts
+++ ts/util/HashMap.ts
@@ -7,19 +7,73 @@ export interface Pair<T extends Hashable, U> {
   value: U;
 }
 
-export class HashMap<T extends Hashable, U> {
-  private hashTable: Array<Array<Pair<T, U>>>;
-  private size: number;
-
-  constructor() {
-    this.hashTable = new Array(12);
-    this.size = 0;
+class HashMapIterator<T extends Hashable, U> implements Iterator<Pair<T, U>> {
+  private tableIterator: Iterator<Array<Pair<T, U>>>;
+  private subtableIterator: Iterator<Pair<T, U>>;
+  private table: Array<Array<Pair<T, U>>>;
+  constructor(table: Array<Array<Pair<T, U>>>) {
+    this.table = table;
+    this.tableIterator = this.table[Symbol.iterator]();
+    // would be neat if we had some global flag to track hash table modifications
+    this.updateSubtableIterator();
   }
 
-  put(key: T, value: U) : Pair<T, U> {
-    if (this.size / this.hashTable.length > MAX_EPSILON) {
+  private updateSubtableIterator() {
+    let subtable : IteratorResult<Array<Pair<T, U>>, Array<Pair<T, U>>>;
+    do {
+      subtable = this.tableIterator.next();
+    } while ((!subtable.value || subtable.value.length <= 0) && !subtable.done);
+
+    if (subtable.done) {
+      // indicate that iteration is done
+      this.subtableIterator = null;
+    } else {
+      this.subtableIterator = subtable.value[Symbol.iterator]();
+    }
+  }
+
+  next() {
+    if (this.subtableIterator === null) {
+      return {
+        done: true,
+        value: null
+      };
+    }
+    let res = this.subtableIterator.next();
+    while (res.done && this.subtableIterator !== null) {
+      this.updateSubtableIterator();
+      if (this.subtableIterator !== null) {
+        res = this.subtableIterator.next();
+      }
+    }
+    
+    return res;
+  }
+}
+
+export class HashMap<T extends Hashable, U> implements Iterable<Pair<T, U>> {
+  private hashTable: Array<Array<Pair<T, U>>>;
+  private size_: number;
+
+  /**
+   * Creates a new HashMap.
+   * @param mod - index size of hash table. defaults to 16.
+   */
+  constructor(mod?: number) {
+    const size = (mod ? mod : 15);
+    this.hashTable = new Array(size);
+    this.size_ = 0;
+  }
+
+  [Symbol.iterator]() : Iterator<Pair<T, U>> {
+    return new HashMapIterator(this.hashTable);
+  }
+
+  put(key: T, value: U) : U {
+    if (this.size_ / this.hashTable.length > MAX_EPSILON) {
       this.rehash();
     }
+
     const hash = key.hash();
     const index = (hash % this.hashTable.length);
 
@@ -40,17 +94,17 @@ export class HashMap<T extends Hashable, U> {
         // duplicate encountered
         const oldVal = dest[i];
         dest[i] = res;
-        return oldVal;
+        return oldVal.value;
       }
     }
 
     // no dupe, insert straight
     dest.push(res);
-    this.size++;
+    this.size_++;
     return null;
   }
 
-  get(key: T) : U {
+  get(key: T) : U | null {
     const index = key.hash() % this.hashTable.length;
     const dest = this.hashTable[index];
     if (!dest) {
@@ -66,14 +120,34 @@ export class HashMap<T extends Hashable, U> {
     return null;
   }
 
-  private rehash() {
-    // create a new map, store the old map
-    // use "put" to reinsert everything into the new hashtable
+  has(key: T) : boolean {
+    const index = key.hash() % this.hashTable.length;
+    const dest = this.hashTable[index];
+    if (!dest) {
+      return false;
+    }
 
+    for (let pair of dest) {
+      if (key.equals(pair.key)) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  get size() {
+    return this.size_;
+  }
+
+  private rehash() {
     const oldTable = this.hashTable;
-    this.hashTable = [];
+    this.hashTable = new Array(Math.round(oldTable.length * MAX_EPSILON));
+    this.size_ = 0;
     for (let chain of oldTable) {
       for (let pair of chain) {
+        // don't do this? we don't have to worry about collisions so we could probably
+        // save some performance
         this.put(pair.key, pair.value);
       }
     }
